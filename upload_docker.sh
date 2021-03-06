62ab0f8917c1450e769b2c143388587308f0f45d
Vikas Bhatia <vikasb@Vikass-MBP.fios-router.home>
2021-03-06 15:41:38 -0500

    all changes for the project

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=vvab13/apitest
 
 # Step 2:  
 # Authenticate & tag
+docker login --username vvab13
+docker tag apitest $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
